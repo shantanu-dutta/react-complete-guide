a810429c45e5ad85fbe9b663890da1911bee284e
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-27 08:02:59 +0530

    1. Changed App component from function to class to use state. 2. Initialised state in App.js. state has property persons which is an array of objects with properties name, age. 3. Looped state persons array to generate Person component for each entry with corresponding entry data.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const app = () => (
-  <div className="App">
-    <h1>Hi, I'm a React App!!!</h1>
-    <Person name="Hari" age="25" />
-    <Person name="Ram" age="27">My Hobbies: Racing</Person>
-    <Person name="Gopal" age="26" />
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Hari', age: 25 },
+      { name: 'Ram', age: 27 },
+      { name: 'Gopal', age: 26 },
+    ]
+  }
 
-  </div>
-);
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!!!</h1>
+        {this.state.persons.map(p => <Person name={p.name} age={p.age} />)}
+      </div>
+    );
+  }
+}
 
-export default app;
+export default App;
