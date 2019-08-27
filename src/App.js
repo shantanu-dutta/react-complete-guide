4a5146b1ec4ad72ee65f6a3f3e93f74abdf902bc
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-27 07:23:29 +0530

    1. Used es6 syntax in App.js. 2. lowercased function name in App.js.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@ import React from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!!!</h1>
-      <Person />
-    </div>
-  );
-}
+const app = () => (
+  <div className="App">
+    <h1>Hi, I'm a React App!!!</h1>
+    <Person />
+  </div>
+);
 
-export default App;
+export default app;
