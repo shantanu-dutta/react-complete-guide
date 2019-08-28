c7e8921c60707f5c5d5a9d9947bd78dd493055c5
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-28 07:31:28 +0530

    1. Reverted App component back to class based component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-const App = props => {
-  const [personsState, setPersonsState] = useState({
+class App extends Component {
+  state = {
     persons: [
       { name: 'Hari', age: 25 },
       { name: 'Ram', age: 27 },
       { name: 'Gopal', age: 26 },
     ],
     otherState: 'some other state'
-  });
+  }
 
-  const [otherState, setOtherState] = useState('some other value');
-
-  console.log(personsState, otherState);
-
-  const switchNameHandler = () => {
-    setPersonsState({
+  switchNameHandler = () => {
+    this.setState({
       persons: [
         { name: 'Haris Chandra', age: 28 },
         { name: 'Ramu', age: 27 },
@@ -26,17 +22,18 @@ const App = props => {
     })
   }
 
-  const personsList = personsState.persons.map(person =>
-    <Person key={person.name + person.age} name={person.name} age={person.age} />
-  );
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App!!!</h1>
-      <button onClick={switchNameHandler}>Switch Name</button>
-      {personsList}
-    </div>
-  );
-};
+  render() {
+    const personsList = this.state.persons.map(person =>
+      <Person key={person.name + person.age} name={person.name} age={person.age} />
+    );
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App!!!</h1>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        {personsList}
+      </div>
+    );
+  }
+}
 
 export default App;
-
