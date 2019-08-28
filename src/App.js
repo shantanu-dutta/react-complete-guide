1f769f818b334ca5d2e2d835084493028cac7c2c
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-28 07:25:25 +0530

    1. Changed App component from class based to functional component. 2. Used useState() for handling state in App component. 3. Used 2 useState for 2 slices of state. 4. Changed App component code to use useState based state handling.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const App = props => {
+  const [personsState, setPersonsState] = useState({
     persons: [
       { name: 'Hari', age: 25 },
       { name: 'Ram', age: 27 },
       { name: 'Gopal', age: 26 },
     ],
     otherState: 'some other state'
-  }
+  });
 
-  switchNameHandler = () => {
-    this.setState({
+  const [otherState, setOtherState] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
+    setPersonsState({
       persons: [
         { name: 'Haris Chandra', age: 28 },
         { name: 'Ramu', age: 27 },
@@ -22,18 +26,17 @@ class App extends Component {
     })
   }
 
-  render() {
-    const personsList = this.state.persons.map(person =>
-      <Person key={person.name + person.age} name={person.name} age={person.age} />
-    );
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App!!!</h1>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        {personsList}
-      </div>
-    );
-  }
-}
+  const personsList = personsState.persons.map(person =>
+    <Person key={person.name + person.age} name={person.name} age={person.age} />
+  );
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App!!!</h1>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      {personsList}
+    </div>
+  );
+};
 
 export default App;
+
