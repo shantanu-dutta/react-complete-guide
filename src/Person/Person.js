f672bee5c507c0c22dcc1666c177e03d93fb9140
Shantanu Dutta <7542135+nlern@users.noreply.github.com>
2019-08-31 10:25:52 +0530

    1. Added media-queries to Person component using inline js styles and using Radium. 2. Wrapped App component in StyleRoot for enabling parsing media-queries by Radium.

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './Person.css';
+import Radium from 'radium';
 
-const person = ({ name, age, children, click, changed }) => (
-  <div className="Person">
-    <p onClick={click}>I'm {name} and I'm {age} years old!</p>
-    <p>{children}</p>
-    <input type="text" onChange={changed} value={name} />
-  </div>
-);
+const person = ({ name, age, children, click, changed }) => {
+  const style = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  };
+  return (
+    <div className="Person" style={style}>
+      <p onClick={click}>I'm {name} and I'm {age} years old!</p>
+      <p>{children}</p>
+      <input type="text" onChange={changed} value={name} />
+    </div>
+  );
+};
 
-export default person;
+export default Radium(person);
